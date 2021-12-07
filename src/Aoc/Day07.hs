d0539a2c09429c695f987f8bb1cfcd865b1864a2
Stoeffel <schtoeffel@gmail.com>
2021-12-07 10:40:28 +0100

    Simplify Day7

--- src/Aoc/Day07.hs
+++ src/Aoc/Day07.hs
@@ -1,5 +1,6 @@
 module Aoc.Day07 (solution) where
 
+import Aoc.Counter (Counter)
 import qualified Aoc.Counter as Counter
 import qualified Aoc.Parser as P
 import qualified Aoc.Solution as S
@@ -13,35 +14,22 @@ parser :: P.Parser (List Int)
 parser = P.csv P.decimal
 
 solution1 :: List Int -> Maybe Int
-solution1 positions =
-  Maybe.map
-    (calcOptimisticFuel positions)
-    (medianPosition positions)
-
-medianPosition :: List Int -> Maybe Int
-medianPosition positions =
-  let half = List.length positions // 2
-   in Data.List.sort positions
-        |> Array.fromList
-        |> Array.get half
-
-calcOptimisticFuel :: List Int -> Int -> Int
-calcOptimisticFuel positions median =
-  positions
-    |> List.map (\position -> abs (position - median))
-    |> List.sum
+solution1 = minimalFuel abs
 
 solution2 :: List Int -> Maybe Int
-solution2 positions =
+solution2 = minimalFuel (abs >> partialSum)
+
+minimalFuel :: (Int -> Int) -> List Int -> Maybe Int
+minimalFuel toFuel positions =
   Maybe.map2 List.range (List.minimum positions) (List.maximum positions)
     |> Maybe.withDefault []
-    |> List.map (calcRealFuel (Counter.fromList positions))
+    |> List.map (calcFuel toFuel (Counter.fromList positions))
     |> List.minimum
 
-calcRealFuel :: Counter.Counter Int -> Int -> Int
-calcRealFuel positions target =
+calcFuel :: (Int -> Int) -> Counter Int -> Int -> Int
+calcFuel toFuel positions target =
   Counter.foldl
-    (\k v acc -> acc + v * partialSum (abs (k - target)))
+    (\k v acc -> acc + v * toFuel (k - target))
     0
     positions
 
